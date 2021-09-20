a83b78ecad67992ade9d88780b0e6be9a3adba0b
Pedro Junqueira <pedrocj@gmail.com>
2021-09-20 19:55:12 +0930

    submit 243

---
+++ 239/test_fizzbuzz.py
@@ -0,0 +1,45 @@
+import random
+
+import pytest
+
+from fizzbuzz import fizzbuzz
+
+
+def test_meet_3_and_5():
+    """
+    given: a function
+    when: a number at sametime divisible by 3 and 5
+    then: return 'Fizz Buzz'
+    """
+    assert fizzbuzz(15) == 'Fizz Buzz'
+
+def test_meet_3():
+    """
+    given: a function
+    when: a number at sametime divisible by 3 and 5
+    then: return 'Fizz'
+    """
+    assert fizzbuzz(3) == 'Fizz'
+
+
+def test_meet_5():
+    """
+    given: a function
+    when: a number divisible by 5
+    then: return 'Buzz'
+    """
+    assert fizzbuzz(5) == 'Buzz'
+
+@pytest.mark.parametrize("rand",[
+            random.choice([ i for i in range(100) if i % 3 != 0 and i % 5 != 0 ]),
+            random.choice([ i for i in range(100) if i % 3 != 0 and i % 5 != 0 ]),
+            random.choice([ i for i in range(100) if i % 3 != 0 and i % 5 != 0 ]),
+            random.choice([ i for i in range(100) if i % 3 != 0 and i % 5 != 0 ]),
+])
+def test_others(rand):
+    """
+    given: a function
+    when: a number not divisible by 3 or 5 
+    then: return 'Buzz'
+    """
+    assert fizzbuzz(rand) == rand
---
+++ 243/run_code.py
@@ -0,0 +1,43 @@
+import os
+from pathlib import Path
+from ipaddress import IPv4Network
+from urllib.request import urlretrieve
+import json
+
+import pytest
+
+from ips import (ServiceIPRange, parse_ipv4_service_ranges,
+                 get_aws_service_range)
+
+URL = "https://bites-data.s3.us-east-2.amazonaws.com/ip-ranges.json"
+TMP = os.getenv("TMP", "/tmp")
+PATH = Path(TMP, "ip-ranges.json")
+IP = IPv4Network('192.0.2.8/29')
+
+
+def json_file():
+    """Import data into tmp folder"""
+    urlretrieve(URL, PATH)
+    return PATH
+
+
+source = json_file()
+
+data = json.loads(source.read_text())
+
+ip_services = parse_ipv4_service_ranges(source)
+
+# print(ip_services)
+
+# print(len(ip_services))
+
+try:
+    ip_range = get_aws_service_range(IP, ip_services)
+except Exception as e:
+    print(e.__dict__)
+
+# print(ip_range)
+
+# sipr = ServiceIPRange(service='WORKSPACES_GATEWAYS', region='eu-central-1',cidr=IP)
+
+# print(sipr)
