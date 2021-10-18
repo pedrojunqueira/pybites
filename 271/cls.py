66fced304d02f7fe67f281e8009da5bd12dd7eee
Pedro Junqueira <pedrocj@gmail.com>
2021-10-18 21:00:24 +1030

    submit 271

---
+++ 271/cls.py
@@ -0,0 +1,6 @@
+import inspect
+
+
+def get_classes(mod):
+    """Return a list of all classes in module 'mod'"""
+    return [name for name, _ in inspect.getmembers(mod, inspect.isclass) if name <= "Z"]
