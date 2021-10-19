2f7ab0a0c27c358be70010eb4c6f82384eac11b8
Pedro Junqueira <pedrocj@gmail.com>
2021-10-20 06:09:28 +1030

    submit 314

---
+++ 314/to_columns.py
@@ -0,0 +1,8 @@
+from typing import List  # not needed when we upgrade to 3.9
+
+TEMPLATE ="| {name}"
+
+def print_names_to_columns(names: List[str], cols: int = 2) -> None:
+    for i in range(0,len(names),cols):
+        line = [f"{TEMPLATE.format(name=name):<12}" for name in names[i:i+cols]]
+        print(''.join(line))
