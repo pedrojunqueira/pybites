3ee873ce514f7a90fa12fc70f8e55a438d1e6376
Pedro Junqueira <pedrocj@gmail.com>
2021-10-20 06:29:56 +1030

    submit 162

--- 162/fill.py
+++ 162/fill.py
@@ -3,4 +3,6 @@ HTML_SPACE = '&nbsp;'
 
 def prefill_with_character(value, column_length=4, fill_char=HTML_SPACE):
     """Prepend value with fill_char for given column_length"""
-    pass
+    value_length = len(str(value))
+    fill_spaces = (column_length - value_length) 
+    return f'{fill_char*fill_spaces}{value}'
