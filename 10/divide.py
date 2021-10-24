81035db30d598adf27e30ddf9a4a2eb0d6d24131
Pedro Junqueira <pedrocj@gmail.com>
2021-10-24 18:21:24 +1030

    submit 10

--- 10/divide.py
+++ 10/divide.py
@@ -1,13 +1,11 @@
 def positive_divide(numerator, denominator):
-    """
-    when denominator is 0 catch the corresponding exception and return 0.
-    when numerator or denominator are not of the right type reraise the corresponding exception.
-    if the result of the division (after surviving the exceptions) is negative, raise a ValueError
-    """
-    if denominator==0:
-        print("zero")
-    if any( n is not isinstance(n,int) or n is not isinstance(n, float) for n in [numerator, denominator]):
-        print("Error")
-
-
-positive_divide(1,0)
+    result = None
+    try:
+        result = numerator/denominator
+    except ZeroDivisionError:
+        return 0
+    except TypeError as e:
+        print(e)
+    if result < 0:
+        raise ValueError
+    return result
