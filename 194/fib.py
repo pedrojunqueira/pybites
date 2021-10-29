2dc963f6a117926dc8bc9678d16ce0e0953d7737
Pedro Junqueira <pedrocj@gmail.com>
2021-10-29 21:23:07 +1030

    submit 194

--- 194/fib.py
+++ 194/fib.py
@@ -1,4 +1,10 @@
 from functools import lru_cache
 
+@lru_cache
 def cached_fib(n):
-    pass
+    if n < 0:
+        raise ValueError
+    elif n in (0, 1):
+        return n
+    else:
+        return(cached_fib(n-1) + cached_fib(n-2))
